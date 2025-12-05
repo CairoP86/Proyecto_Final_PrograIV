3ee1e5efccb4e6917c89dc9be06c87c1ff325efa
Kevin <kevinquirosh26@gmail.com>
2025-12-04 19:23:12 -0600

    asignacionSoftware.V1

--- Asignacion de Software.Designer.cs
+++ Asignacion de Software.Designer.cs
@@ -30,93 +30,93 @@
         {
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.cbmCampo = new System.Windows.Forms.ComboBox();
-            this.cbmDato = new System.Windows.Forms.ComboBox();
+            this.cbmSoftware = new System.Windows.Forms.ComboBox();
+            this.cbmComputadora = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.txtMarca = new System.Windows.Forms.TextBox();
-            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
             this.btnInstalar = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label8);
-            this.groupBox2.Controls.Add(this.cbmCampo);
-            this.groupBox2.Controls.Add(this.cbmDato);
+            this.groupBox2.Controls.Add(this.cbmSoftware);
+            this.groupBox2.Controls.Add(this.cbmComputadora);
             this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.txtMarca);
-            this.groupBox2.Controls.Add(this.cmbTipo);
+            this.groupBox2.Controls.Add(this.txtFecha);
             this.groupBox2.Controls.Add(this.btnInstalar);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(38, 87);
+            this.groupBox2.Location = new System.Drawing.Point(28, 71);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(400, 337);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(300, 221);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 71);
+            this.label8.Location = new System.Drawing.Point(27, 29);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(147, 16);
+            this.label8.Size = new System.Drawing.Size(119, 13);
             this.label8.TabIndex = 16;
             this.label8.Text = "Seleccione el Software:";
             // 
-            // cbmCampo
+            // cbmSoftware
             // 
-            this.cbmCampo.FormattingEnabled = true;
-            this.cbmCampo.Location = new System.Drawing.Point(184, 26);
-            this.cbmCampo.Name = "cbmCampo";
-            this.cbmCampo.Size = new System.Drawing.Size(166, 24);
-            this.cbmCampo.TabIndex = 13;
+            this.cbmSoftware.FormattingEnabled = true;
+            this.cbmSoftware.Location = new System.Drawing.Point(151, 26);
+            this.cbmSoftware.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbmSoftware.Name = "cbmSoftware";
+            this.cbmSoftware.Size = new System.Drawing.Size(126, 21);
+            this.cbmSoftware.TabIndex = 13;
             // 
-            // cbmDato
+            // cbmComputadora
             // 
-            this.cbmDato.FormattingEnabled = true;
-            this.cbmDato.Location = new System.Drawing.Point(184, 71);
-            this.cbmDato.Name = "cbmDato";
-            this.cbmDato.Size = new System.Drawing.Size(166, 24);
-            this.cbmDato.TabIndex = 14;
+            this.cbmComputadora.FormattingEnabled = true;
+            this.cbmComputadora.Location = new System.Drawing.Point(151, 58);
+            this.cbmComputadora.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbmComputadora.Name = "cbmComputadora";
+            this.cbmComputadora.Size = new System.Drawing.Size(126, 21);
+            this.cbmComputadora.TabIndex = 14;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 29);
+            this.label6.Location = new System.Drawing.Point(7, 61);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(135, 16);
+            this.label6.Size = new System.Drawing.Size(140, 13);
             this.label6.TabIndex = 15;
-            this.label6.Text = "Seleccione el Equipo";
+            this.label6.Text = "Seleccione la Computadora:";
             // 
-            // txtMarca
+            // txtFecha
             // 
-            this.txtMarca.Location = new System.Drawing.Point(185, 107);
-            this.txtMarca.Name = "txtMarca";
-            this.txtMarca.Size = new System.Drawing.Size(165, 22);
-            this.txtMarca.TabIndex = 9;
-            // 
-            // cmbTipo
-            // 
-            this.cmbTipo.FormattingEnabled = true;
-            this.cmbTipo.Location = new System.Drawing.Point(185, 151);
-            this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(165, 24);
-            this.cmbTipo.TabIndex = 8;
+            this.txtFecha.Location = new System.Drawing.Point(151, 87);
+            this.txtFecha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(127, 20);
+            this.txtFecha.TabIndex = 9;
             // 
             // btnInstalar
             // 
-            this.btnInstalar.Location = new System.Drawing.Point(185, 210);
+            this.btnInstalar.Location = new System.Drawing.Point(54, 161);
+            this.btnInstalar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInstalar.Name = "btnInstalar";
-            this.btnInstalar.Size = new System.Drawing.Size(165, 28);
+            this.btnInstalar.Size = new System.Drawing.Size(196, 41);
             this.btnInstalar.TabIndex = 6;
             this.btnInstalar.Text = "Instalar";
             this.btnInstalar.UseVisualStyleBackColor = true;
@@ -124,35 +124,40 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 110);
+            this.label5.Location = new System.Drawing.Point(51, 87);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(118, 16);
+            this.label5.Size = new System.Drawing.Size(96, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Fecha instalacion :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 154);
+            this.label4.Location = new System.Drawing.Point(37, 123);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 16);
+            this.label4.Size = new System.Drawing.Size(109, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Tecnico que instaló  :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 233);
+            this.label2.Location = new System.Drawing.Point(7, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 1;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(64, 36);
+            this.groupBox1.Location = new System.Drawing.Point(28, 29);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(353, 45);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(300, 37);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             // 
@@ -160,22 +165,33 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Black", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(60, 18);
+            this.label1.Location = new System.Drawing.Point(59, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 17);
+            this.label1.Size = new System.Drawing.Size(178, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Proceso de Instalacion Software";
             // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(151, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 17;
+            // 
             // Asignacion_de_Software
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(491, 439);
+            this.ClientSize = new System.Drawing.Size(368, 309);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Asignacion_de_Software";
             this.Text = "Asignacion_de_Software";
+            this.Load += new System.EventHandler(this.Asignacion_de_Software_Load);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
@@ -187,16 +203,16 @@
         #endregion
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.ComboBox cbmCampo;
-        private System.Windows.Forms.ComboBox cbmDato;
+        private System.Windows.Forms.ComboBox cbmSoftware;
+        private System.Windows.Forms.ComboBox cbmComputadora;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtMarca;
-        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.TextBox txtFecha;
         private System.Windows.Forms.Button btnInstalar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Asignacion de Software.cs
+++ Asignacion de Software.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,68 @@ namespace Proyecto_Final_PrograIV
         public Asignacion_de_Software()
         {
             InitializeComponent();
+        }
+
+        private void cargarSoftware()
+        {
+            try
+            {
+                using (SqlConnection con = Conexion.ObtenerConexion())
+                {
+                    con.Open();
+
+                    string query = "SELECT IdSoftware, Nombre FROM Software";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(dr);
+
+                    cbmSoftware.DataSource = dt;
+                    cbmSoftware.DisplayMember = "Nombre";    
+                    cbmSoftware.ValueMember = "IdSoftware";  
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error cargando software: " + ex.Message);
+            }
+        }
+
+        private void cargarCompus()
+        {
+            try
+            {
+                using (SqlConnection con = Conexion.ObtenerConexion())
+                {
+                    con.Open();
+
+                    string query = "SELECT Computadoras, Marca FROM Computadoras";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(dr);
+
+                    cbmComputadora.DataSource = dt;
+                    cbmComputadora.DisplayMember = "Marca";
+                    cbmComputadora.ValueMember = "IdComputadora";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error cargando software: " + ex.Message);
+            }
+        }
+
+        private void Asignacion_de_Software_Load(object sender, EventArgs e)
+        {
+            cargarSoftware();
+            cargarCompus();
+            txtFecha.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            //Falta jalar el login actual 
         }
     }
 }
